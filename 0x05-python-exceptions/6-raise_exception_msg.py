c4198c27e817515bfbb1c9d05e5de1fe7faa507d
Emmanuelngato <emmanuelngato242@gmail.com>
2024-01-22 06:41:47 -0800

    Python exceptions

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,5 +1,8 @@
-g!/usr/bin/python3
+#!/usr/bin/python3
 
 def raise_exception_msg(message=""):
-    """Raise a NameError exception with a message."""
     raise NameError(message)
+try:
+    raise_exception_msg("C is fun")
+except NameError as ne:
+    print(ne)
